b7afc77604e92b949d11568a5186526e6330696f
takeokunn <bararararatty@gmail.com>
2026-09-01 18:43:24 +0900

    test: cover file diff result handling

--- tests/unit/bootstrap/server-dispatch-helper-tests.lisp
+++ tests/unit/bootstrap/server-dispatch-helper-tests.lisp
@@ -1208,6 +1208,32 @@
                        (gethash (list "callback-refresh" "README.md")
                                 nerimux::*workspace-file-diffs*))))))
 
+  (it "stores successful and unsuccessful file diff results from CPS"
+    (let* ((worktree (nerimux/workspace-model:make-worktree
+                      :id "callback-results" :path "/tmp/callback-results"
+                      :branch "main"))
+           (nerimux::*workspace-file-diffs* (make-hash-table :test #'equal))
+           (nerimux::*dirty* nil)
+           (results (list (list :ready 7 "+added" "-removed")
+                          (list :unexpected 0 nil))))
+      (with-stubbed-fdefinition
+          ((nerimux/vcs:refresh-worktree-file-diff-async
+             (lambda (&rest arguments)
+               (let ((on-complete (getf (cdddr arguments) :on-complete)))
+                 (funcall on-complete (pop results)))))
+           (nerimux::%mark-dirty (lambda () (setf nerimux::*dirty* t))))
+        (nerimux::%client-start-worktree-file-diff-refresh
+         worktree "README.md")
+        (nerimux::%client-start-worktree-file-diff-refresh
+         worktree "CHANGELOG.md")
+        (expect (equal (list :ready 7 (list "+added" "-removed"))
+                       (gethash (list "callback-results" "README.md")
+                                nerimux::*workspace-file-diffs*)))
+        (expect (equal (list :failed 0 nil)
+                       (gethash (list "callback-results" "CHANGELOG.md")
+                                nerimux::*workspace-file-diffs*)))
+        (expect nerimux::*dirty*))))
+
   )
 
 ;;; PR2 tree-navigation redesign (R6.3 pivot): Enter on a repository row dives
