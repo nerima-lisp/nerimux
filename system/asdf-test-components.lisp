bd84b9867a89c3b134f67d700bdc935e17cb29a5
takeokunn <bararararatty@gmail.com>
2026-09-04 13:40:07 +0900

    test: split server multi command specs

--- system/asdf-test-components.lisp
+++ system/asdf-test-components.lisp
@@ -62,6 +62,7 @@
          (:file "server-multi-tests-support")
          (:file "server-multi-tests-size")
          (:file "server-multi-tests-message-dispatch-selection")
+         (:file "server-multi-tests-message-dispatch-commands")
          (:file "server-multi-tests-message-dispatch-rendering")
          (:file "server-multi-tests-message-dispatch")
          (:file "server-multi-tests-transient")
